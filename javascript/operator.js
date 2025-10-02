532314b70b790c5cd9d98bb29cc8c19a54e37a14
Sobia3484 <sobiabibighafoor@gmail.com>
2025-10-02 15:09:41 +0800

    operator

---
+++ javascript/operator.js
@@ -0,0 +1,68 @@
+// operator
+
+// 1. arithmetic operator
+
+// let a=5;
+// let b=9;
+
+// console.log("Additional operator:",a+b);
+// console.log("Subtraction:",a-b);
+// console.log("Multiplication",a*b);
+// console.log("Division",a/b);
+// console.log("Modulus (Reminder)",a%b);
+// console.log("Exponentiation (Power)",a**b);
+
+
+// Assignment operator
+
+
+// let a = 10;      //simple asssign
+
+//add and assign
+// let x = 10;
+// x+=5;
+// console.log(x);
+
+// //subtraction
+// x-=3;
+// console.log(x);
+
+// //multiplication
+// x*=9;
+// console.log(x);
+
+// //division
+// x/=7;
+// console.log(x);
+
+// //modulus
+// x%=8;
+// console.log(x);
+
+// //exponentional
+// x**=5;
+// console.log(x);
+
+                      //compersion operator
+//1.equal
+// console.log(5=="5");
+
+//2.strict equal (type must be same)
+
+// console.log(5==="5");    //return false
+// console.log(5===5);      //return true
+
+//3.not equal
+// console.log(5!=10);/
+
+//4.strict not equal
+// console.log(5!=="6");
+
+
+
+
+
+
+
+
+
